1aa8c73b020ed635beea320e69e52335175ff02d
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-14 20:41:10 -0200

    Update midi.js

--- midi.js
+++ midi.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const parse = require('midi-file-parser');
 
-const file = fs.readFileSync(process.argv[4] ? process.argv[4] : 'octave_demo.mid', 'binary');
+const fname = process.argv[4] ? process.argv[4] : 'octave_demo.mid';
+const file = fs.readFileSync(fname, 'binary');
 const parsed = parse(file);
-// fs.writeFileSync('octave_demo.json', JSON.stringify(parsed, ' ', 4));
+fs.writeFileSync(fname + '.json', JSON.stringify(parsed, ' ', 4));
 
 const { Zz } = require('./notes');
 
@@ -21,13 +22,14 @@ for(let event of parsed.tracks[process.argv[5] != null ? process.argv[5] : 0]){
     if(event.subtype == 'noteOn' || event.subtype == 'noteOff'){
         const note = (event.noteNumber - 24) % 12;
         const octave = Math.floor((event.noteNumber - 24) / 12);
-        // console.log('PLAY: ' + event.subtype + '. Note: ' + notes[note]);
 
         if(event.subtype == 'noteOn'){
             if(playing)
                 song.notes[song.notes.length - 1][2] = event.deltaTime;
             else if(event.deltaTime > 0)
                 song.notes.push([ Zz, 0, event.deltaTime ]);
+
+            // console.log('Note: ' + notes[note] + octave);            
 
             song.notes.push([ note, octave, 0 ]);
 
@@ -40,11 +42,15 @@ for(let event of parsed.tracks[process.argv[5] != null ? process.argv[5] : 0]){
     }
 }
 
+song.notes = song.notes.map(x => {
+    x[2] *= 307.692;
+    return x;
+});
+
 song.notes.push([ -1, -1, -1 ]);
 
 // fs.writeFileSync('n.json', JSON.stringify(song, ' ', 4));
 
-// console.log(song);
 const Floppy = require('./floppy');
 const drive = new Floppy(parseInt(process.argv[2]), parseInt(process.argv[3]));
 drive.playSong(song.notes, song.tempo);
