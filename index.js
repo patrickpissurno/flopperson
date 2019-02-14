1677f64ad174d06a1d1f3ce1af7c54bce507eaac
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-14 16:15:19 -0200

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const gpio = require('rpi-gpio');
-(async () => {
-    await gpio.setup('1', gpio.DIR_HIGH);
-    await gpio.write('1', true);
-    console.log('DONE');
-})();
+const { Gpio } = require('onoff');
+const pin17 = new Gpio(17, 'out');
+pin17.writeSync(1);
