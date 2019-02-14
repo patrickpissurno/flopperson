4c70fd741ac108c7231614bb9df23cedf4655d12
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-14 15:57:15 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const gpio = require('rpi-gpio');
 gpio.setup(1, gpio.DIR_HIGH, (err) => {
+    console.log('HERE (2)');
     if(err)
         throw err;
-
-    gpio.write(1, true);
+        
+    gpio.write(1, true, () => {
+        console.log('HERE (2)');
+    });
 });
