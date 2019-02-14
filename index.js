5939a58311e4652a5ad94a1d99dbf954bf1a5cfc
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-14 17:19:31 -0200

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ class Floppy {
     }
 }
 
-const drive1 = new Floppy(parseInt(process.argv[1]), parseInt(process.argv[2]));
+const drive1 = new Floppy(parseInt(process.argv[2]), parseInt(process.argv[3]));
 // const drive2 = new Floppy(22, 23);
 const song = require('./hardcoded_songs/imperial_march');
 drive1.playSong(song.notes, song.tempo);
