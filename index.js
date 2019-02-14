a75df515608ba6c56a41d4e455b8bdc9a6f36399
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-14 16:25:54 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 const { Gpio } = require('onoff');
-const pin17 = new Gpio(17, 'out');
-const pin18 = new Gpio(18, 'out');
-pin17.writeSync(1);
-pin18.writeSync(1);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class Floppy {
+    constructor(directionPin, stepPin){
+        this.directionPin = new Gpio(directionPin, 'out');
+        this.stepPin = new Gpio(stepPin, 'out');
+
+        this.resetMotor();
+    }
+
+    async resetMotor(){
+        this.directionPin.writeSync(0);
+        for(let i = 0; i < 10; i++){
+            this.stepPin.writeSync(1);
+            this.stepPin.writeSync(0);
+            await sleep(1);
+        }
+        
+        this.directionPin.writeSync(1);
+        
+        for(let i = 0; i < 10; i++){
+            this.stepPin.writeSync(1);
+            this.stepPin.writeSync(0);
+            await sleep(1);
+        }
+
+        await sleep(400);
+    }
+}
+
+const drive1 = new Floppy(17, 18);
