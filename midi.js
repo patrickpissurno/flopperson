eb28822531a3eb738ddd873b7a34f4b699629048
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-02-15 03:24:48 -0200

    Update midi.js

--- midi.js
+++ midi.js
@@ -60,13 +60,7 @@ async function parseMidi(fname = 'octave_demo.mid', track_id = -1, opts = { tran
 
             if(event.subtype == 'noteOn'){
                 if(playing){
-                    if(!opts.forcePlayingAllNotes){
-                        song.notes[song.notes.length - 1][2] += event.deltaTime;
-                        playing = null;
-                        // continue;
-                    }
-                    else
-                        song.notes[song.notes.length - 1][2] = event.deltaTime;
+                    song.notes[song.notes.length - 1][2] = event.deltaTime;
                 }
                 else if(event.deltaTime > 0)
                     song.notes.push([ Zz, 0, event.deltaTime ]);       
